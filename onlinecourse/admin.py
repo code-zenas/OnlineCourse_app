6a3eb855b098586ebe4890d4dc8f1bb3f04e5078
code-zenas <geenesh01@gmail.com>
2026-05-16 15:53:16 -0500

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,1 +1,40 @@
+from django.contrib import admin
 
+from .models import (
+    Course,
+    Lesson,
+    Enrollment,
+    Question,
+    Choice,
+    Submission,
+)
+
+# Choice Inline
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+# Question Inline
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+
+# Question Admin
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+# Lesson Admin
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course', 'order']
+
+
+# Register Models
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Enrollment)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
