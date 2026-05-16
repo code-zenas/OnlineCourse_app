3f8a631cf5959457ad564269cadf805a35e0917c
code-zenas <geenesh01@gmail.com>
2026-05-16 16:04:34 -0500

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -7,34 +7,32 @@ from .models import (
     Question,
     Choice,
     Submission,
+    Instructor,
 )
 
-# Choice Inline
+
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 2
 
 
-# Question Inline
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 2
 
 
-# Question Admin
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
 
-# Lesson Admin
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title', 'course', 'order']
 
 
-# Register Models
 admin.site.register(Course)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Enrollment)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
+admin.site.register(Instructor)
